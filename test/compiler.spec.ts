b8af0812fabde5c293784b34b0c34d421c3ed8cf
krigga <krigga7@gmail.com>
2023-03-13 18:08:18 +0300

    chore: bump func-js-bin to v0.4.2, bump version

--- test/compiler.spec.ts
+++ test/compiler.spec.ts
@@ -6,7 +6,7 @@ describe('func-js', () => {
     const walletCodeCellHash = Buffer.from("hA3nAz+xEJePYGrDyjJ+BXBcxSp9Y2xaAFLRgGntfDs=", 'base64');
 
     const compilerVersionExpected = {
-        funcVersion: "0.4.1",
+        funcVersion: "0.4.2",
     };
 
     it('should return compiler version', async () => {
